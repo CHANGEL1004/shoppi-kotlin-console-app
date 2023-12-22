70798ecef3ce9cdde5c9bbb81c69b7b1b300a3a1
CHANGEL <107169027+CHANGEL1004@users.noreply.github.com>
2023-12-23 01:06:01 +0900

    [Feat] : 바텀 네비게이션 관련 기능 구현

---
+++ app/src/main/java/com/shoppi/app/CategoryFragment.kt
@@ -0,0 +1,18 @@
+package com.shoppi.app
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import java.util.zip.Inflater
+
+class CategoryFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return layoutInflater.inflate(R.layout.fragment_category, container, false)
+    }
+}
